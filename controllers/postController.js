834d58f30e05645940d4c11e54ec4012b60f7a1d
shreekanth senthil <shreerushmi@gmail.com>
2020-05-12 21:26:37 +0530

    Added delete

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,17 +26,17 @@ exports.viewSingle = async function(req, res) {
 
 exports.viewEditScreen = async function(req, res) {
     try {
-        let post = await Post.findSingleById(req.params.id)
-        if(post.authorId == req.visitorId) {
-            res.render('edit-post', {post: post})
-        } else {
-            req.flash("errors", "You do not have permission to perform that action.")
-            req.session.save(() => res.redirect('/'))
-        }
+      let post = await Post.findSingleById(req.params.id, req.visitorId)
+      if (post.isVisitorOwner) {
+        res.render("edit-post", {post: post})
+      } else {
+        req.flash("errors", "You do not have permission to perform that action.")
+        req.session.save(() => res.redirect("/"))
+      }
     } catch {
-        res.render('404')
+      res.render("404")
     }
-} 
+  }
 
 exports.edit = function(req, res) {
     let post = new Post(req.body, req.visitorId, req.params.id)
@@ -61,5 +61,15 @@ exports.edit = function(req, res) {
         req.session.save(function() {
         res.redirect("/")
     })        
+    })
+}
+
+exports.delete = function(req, res) {
+    Post.delete(req.params.id, req.visitorId).then(() => {
+        req.flash("success", "Post successfully deleted.")
+        req.save(() => res.redirect(`/profile/${req.session.user.username}`))
+    }).catch(() => {
+        req.flash("errors", "You do not have permission to perform that action")
+        req.session.save(() => res.redirect('/'))
     })
 }
--- models/Post.js
+++ models/Post.js
@@ -129,4 +129,20 @@ Post.findByAuthorId = function(authorId) {
     ])
 }
 
+Post.delete = function(postIdToDelete, currentUserId) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let post = await Post.findSingleById(postIdToDelete, currentUserId)
+            if (post.isVisitorOwner) {
+                await postCollection.deleteOne({_id: new ObjectID(postIdToDelete)})
+                resolve()
+            } else {
+                reject()
+            }
+        } catch {
+            reject()
+        }
+    })
+}
+
 module.exports = Post
