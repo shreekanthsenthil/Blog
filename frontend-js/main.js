013ab98b6d933556532f3487b20995be4dfdd610
shreekanth senthil <shreerushmi@gmail.com>
2020-05-24 19:57:28 +0530

    Added Chat Feature

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,3 +1,7 @@
 import Search from './modules/search'
+import Chat from './modules/chat'
 
+if(document.querySelector("#chat-wrapper")) {
+    new Chat()
+}
 if(document.querySelector('.header-search-icon')) { new Search() }
